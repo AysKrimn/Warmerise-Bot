30a2f970e8b55f010c2c8873b632a475e5fc8ef5
_omerock <83617943+AysKrimn@users.noreply.github.com>
2022-07-31 08:35:14 +0300

    Update info.js

--- src/interactions/info.js
+++ src/interactions/info.js
@@ -42,7 +42,8 @@ module.exports = {
     const url = encodeURI(path);
 
     const request = await fetch(url).catch(e => {});
-        
+
+    console.log("STATUS CODE:", request.status);
     if(request.status == 403) return constants.interaction.editReply("I don't have permission to view this private page.");
 
     if(request.ok) {
@@ -62,6 +63,8 @@ module.exports = {
     return $(this).text() === 'Rank';
     }).next().text().trim();
         
+    let rankRef = $('table td').not('id').find('a').attr('href');
+
     let xp = $('.row-title').filter(function() {
     return $(this).text() === 'XP';
     }).next().text().trim();
@@ -85,7 +88,8 @@ module.exports = {
     let tableContent = $(li).find('.datagrid').children().text().trim();
 
     let weaponStats = handleStats(tableContent);
-    
+
+
     // KLANLAR VE TOPLAM KLAN SAYISI BOLUMU BURADA BASLAR
 
     let clans = [];
@@ -111,9 +115,9 @@ module.exports = {
      if(tabContent.length == 1) totalClan = tabContent[0].match(pattern);
      if(tabContent.length > 1) totalClan = tabContent[1].match(pattern);
 
-     if(totalClan && totalClan > 5) clans.push(` ${totalClan - 5} more.`); 
+    if(totalClan && totalClan > 5) clans.push(` ${totalClan - 5} and more.`); 
 
-     } else clans.push("Not a member of any clan.");
+    } else clans.push("Not a member of any clan.");
 
     // KLANLAR VE TOPLAM KLAN SAYISI BOLUMU BITER
     
@@ -138,6 +142,12 @@ module.exports = {
 
     });
 
+
+    let goTo;
+
+    if(rankRef) goTo = `https://warmerise.com/${rankRef}`;
+
+
     switch(rank) {
     case "Not ranked yet":
     color = "ORANGE"
@@ -149,18 +159,26 @@ module.exports = {
     break;
 
     case "1":
-    rank = "🥇";
+    rank = `[${"🥇"}](${goTo})`;
     break;
 
     case "2":
-    rank = "🥈";
+    rank = `[${"🥈"}](${goTo})`;
     break;
 
     case "3":
-    rank = "🥉";
+    rank = `[${"🥉"}](${goTo})`;
+
+    break;
+
+    default: 
+
+    if(rank >= 4 && rank <= 100) rank = `🏅 [${rank}](${goTo})`;
+    if(rank > 100) rank = `[${rank}](${goTo})`;
+
     }
 
-    if(rank >= 4 && rank <= 100) rank = `🏅 ${rank}`;
+
     if(isApril) rank = "**Banned**"; 
 
     const embed = new MessageEmbed()
@@ -168,6 +186,7 @@ module.exports = {
     .setDescription(`>>> User Stats\nRank: ${rank}\nXP: ${xp}\nKills: ${kills}\nDeaths: ${deaths}\nKDR: ${kdr}\nHighest Killstreak: ${hk}\nFavourite Weapon: ${weaponStats.mostKills}`)
     .setThumbnail(`${baseURL}/${image}`)
     .setFooter({ text: footer });
+
     // clan section false değilse clanları göster
     if(clans[0] != false) embed.description += `\nClan(s): ${clans.join(' ')}`;
     // badgeler varsa badgeleri göster
@@ -193,10 +212,12 @@ module.exports = {
         const request = await fetch(url);
     
         if(request.ok) {
-            
+
         let n;
+
         const html = await request.text().catch(e => {});
         const $ = cheerio.load(html);
+
 
         const results = $('#browsemembers_results').find('h3').text().trim().split(' ');
 
@@ -209,9 +230,12 @@ module.exports = {
         const u_name = $(element).find('a').text().trim();
         const u_link = $(element).find('a').attr('href');
 
+
         foundUsers.push(`• [${u_name}](${baseURL}${u_link})`);
 
         });
+
+        console.log("search:", foundUsers);
 
         if(!foundUsers.length) return constants.interaction.editReply("Could not find requested player.");
      
@@ -221,10 +245,12 @@ module.exports = {
         .setColor('GREEN')
         .addField("Search Results", foundUsers.join('\n').toString())
         .setTimestamp();
-            
 
-        if(Number(n) > 10) { embed.setTitle(`${n} match`); embed.setURL(path) }
-            
+
+        const ref = `https://warmerise.com/members?displayname=${userName}`;
+
+        if(Number(n) > 10) { embed.setTitle(`${n} match`); embed.setURL(ref) }
+
         constants.interaction.editReply({ embeds: [embed]});
         
 
