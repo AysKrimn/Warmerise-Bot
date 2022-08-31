4f4cea1388e19513edd17b497877ce5cac48ef59
_omerock <83617943+AysKrimn@users.noreply.github.com>
2022-08-31 07:48:44 +0300

    Update handleStats.js

--- functions/handleStats.js
+++ functions/handleStats.js
@@ -1,81 +1,40 @@
 // @Warmerise Fav Weapon Stats
 
-function stats(tableContent) {
-    const output = {};
-    const asArray = tableContent.replace(/(\t|\n)/gm, ' ').split(' ').filter(d => d);
-    console.log("Fonksiyon:", asArray);
-    
-    const freeItems = asArray.indexOf('Free');
-    const purchasedItems = asArray.indexOf('Purchased');
-    
-    /*
-    favoriteWeapons = silahların ismi alttakiler ise kill-count.
-    MFX = Most Free XP
-    MPX = Most Purchased XP
-    */
-    let favouriteFreeWeapon = asArray[freeItems + 4];
-    let MFX = asArray[freeItems + 5];
-    
-    let favouritePurchasedWeapon = asArray[purchasedItems + 4];
-    let MPX = asArray[purchasedItems + 5];
-    
-    switch(favouriteFreeWeapon) {
-    case "UMP":
-    favouriteFreeWeapon = "UMP 40"; 
-    MFX = asArray[freeItems + 6];
-    break;
-    
-    case "Vector":
-    favouriteFreeWeapon = "Vector X";
-    MFX = asArray[freeItems + 6];
-    break;
-    
-    case "Blast":
-    favouriteFreeWeapon = "Blast Shotgun";
-    MFX = asArray[freeItems + 6];
-    }
-    
-    output['FreeWeapon'] = favouriteFreeWeapon;
-    output['FXP'] = MFX;
-    
-    // eğer satın alınmış silahlar varsa
-    if(favouritePurchasedWeapon) {
-    
-    switch(favouritePurchasedWeapon) {
-    
-    case "Vector":
-    favouritePurchasedWeapon = "Vector X";
-    MPX = asArray[purchasedItems + 6];
-    break;
-    
-    case "Blast":
-    favouritePurchasedWeapon = "Blast Shotgun";
-    MPX = asArray[purchasedItems + 6];
-    
-    }
-    
-    output['PurchasedWeapon'] = favouritePurchasedWeapon
-    output['PXP'] = MPX;
-    }
-      
-    if(!favouritePurchasedWeapon) MPX = '0';
-      
-    console.log("F:", favouriteFreeWeapon, "P:", favouritePurchasedWeapon);
-    
-      
-    // fav silahi ayarla
-    if(Number(MFX) > 100 || Number(MPX) > 100)  {
-    if(Number(MFX) > Number(MPX)) output.mostKills = favouriteFreeWeapon;
-    else output.mostKills = favouritePurchasedWeapon;
-    
-    } else output.mostKills = "Not yet calculated.";
+function stats($) {
+/* eq(0) silah adi eq(2) öldürmeler eq(3) ise silah xp'sine eşit */
+
+ let output = {};
+
+ const freeContent = $('tbody').eq(1);
+ const f_weaponName = freeContent.find('tr td').eq(0).text()
+ const f_weaponKills = Number(freeContent.find('tr td').eq(2).text())
+
+
+ const purchasedContent = $('tbody').last();
+ const p_weaponName = purchasedContent.find('tr td').eq(0).text()
+ const p_weaponKills = Number(purchasedContent.find('tr td').eq(2).text())
+
+ console.log("from fn:", f_weaponName, f_weaponKills)
+ console.log("from fn:", p_weaponName, p_weaponKills)
+
+ if(f_weaponKills > p_weaponKills) output.mostKills = f_weaponName
+ else output.mostKills = p_weaponName
+ 
+
+ if(f_weaponKills > 100 || p_weaponKills > 100) return output
+
+ else {
+ output.mostKills = "Not yet calculated"
+ return output
+
+ }
+
+
     
     
-    return output;
-    
-    }
+}
     
     
     
     
-    module.exports = stats;
+module.exports = stats;
