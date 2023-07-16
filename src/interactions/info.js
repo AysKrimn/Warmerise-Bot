e786df59986b12cb389d522dca62eff550f10a42
_omerock <83617943+AysKrimn@users.noreply.github.com>
2023-07-16 20:54:08 +0300

    Update info.js

--- src/interactions/info.js
+++ src/interactions/info.js
@@ -1,15 +1,20 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const cheerio = require('cheerio');
-const handleStats = require('../../functions/handleStats');
-const handleApril = require('../../functions/april');
 
+const handleStats = require('../../functions/handleStats');
+const getUserId = require('../../functions/script');
+const get_or_create_log = require('../../functions/handleRegistery')
 const { escapeMarkDown } = require('../../functions/escapeCharacters');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 
+//  TODO:
+// add, get fonksiyonu eklenecek ve user id üzerinden name tracker.
 
 const baseURL = "https://warmerise.com";
+
+
 
 module.exports = {
     botPermissions: ["READ_MESSAGE_HISTORY", "EMBED_LINKS"],
@@ -18,19 +23,36 @@ module.exports = {
     data: new SlashCommandBuilder()
     .setName('info')
     .setDescription("Returns specified user's profile")
+
     .addStringOption(option =>
      option.setName('username')
     .setDescription("User's Warmerise profile name")
-    .setRequired(true)),
+    .setRequired(true))
+
+    .addStringOption(option =>
+		option.setName('stats')
+			.setDescription("Option to display users yearly or all time stats.")
+            .setRequired(true)
+            .addChoice('This Year', 'this_y')
+            .addChoice('All Time', "all_t")),
 
 
     async run(client, constants) {
-    await constants.interaction.deferReply();
+    await constants.interaction.deferReply().catch(e => {});
 
-    const isApril = handleApril();
-
+    let query;
+    let e_title = "User Stats";
     let userName = constants.interaction.options.getString('username');
+
     const isUrl = userName.startsWith('https');
+    const userStats = constants.interaction.options.getString('stats');
+
+
+    if(userStats == "this_y") {
+    query = ".this_year"
+    e_title += " (This Year)"
+
+    } else query = ".all_time";
 
     if(isUrl) {
     const parseUrl = userName.split('/');
@@ -47,45 +69,54 @@ module.exports = {
     if(request.status == 403) return constants.interaction.editReply("I don't have permission to view this private page.");
 
     if(request.ok) {
+
+    console.log("req",request)
     // burası bize html'i verir.
     const html = await request.text().catch(e => {});
     const $ = cheerio.load(html);
 
+    let profileName = $("#profile_status h2").text().trim();
+    const userId = getUserId($);
+    console.log("USER ID:", userId)
+    const userNames = await get_or_create_log({userId: userId, currentName: profileName})
+    console.log("userin logları:", userNames)
     const listItems = $(".layout_page_user_profile_index"); 
 
     listItems.each(function(idx, li) {
 
     let color = "GREEN";
-    let image =  $(li).find('img').attr('src'); 
-    let profileName = $(li).find('h2').text().trim();
+    let image = $(li).find('img').attr('src'); 
 
-    let rank = $('.row-title').first(function() {
+    let rankRef = $(query + " td").not('id').find('a').attr('href');
+    
+    query += " .row-title";
+
+    let rank = $(query).first(function() {
     return $(this).text() === 'Rank';
     }).next().text().trim();
-        
-    let rankRef = $('table td').not('id').find('a').attr('href');
 
-    let xp = $('.row-title').filter(function() {
+    let xp = $(query).filter(function() {
     return $(this).text() === 'XP';
     }).next().text().trim();
         
-    let kills = $('.row-title').filter(function() {
+    let kills = $(query).filter(function() {
     return $(this).text() === 'Kills';
     }).next().text().trim();
         
-    let deaths = $('.row-title').filter(function() {
+    let deaths = $(query).filter(function() {
     return $(this).text() === 'Deaths';
     }).next().text().trim();
         
-    let kdr = $('.row-title').filter(function() {
+    let kdr = $(query).filter(function() {
     return $(this).text() === 'KDR';
     }).next().text().trim();
         
-    let hk = $('.row-title').filter(function() {
+    let hk = $(query).filter(function() {
     return $(this).text() === 'Highest Killstreak';
     }).next().text().trim();
 
-    let weaponStats = handleStats($);
+
+    let weaponStats = handleStats($, userStats)
 
 
     // KLANLAR VE TOPLAM KLAN SAYISI BOLUMU BURADA BASLAR
@@ -112,6 +143,8 @@ module.exports = {
      // tabcontent 1. dizin sadece rakamı verir.
      if(tabContent.length == 1) totalClan = tabContent[0].match(pattern);
      if(tabContent.length > 1) totalClan = tabContent[1].match(pattern);
+  
+    console.log("clan sayisi:", totalClan);
 
     if(totalClan && totalClan > 5) clans.push(` ${totalClan - 5} and more.`); 
 
@@ -135,6 +168,8 @@ module.exports = {
 
     const place = $(element).find('a').attr('title');
     const e_link = $(element).find('a').attr('href');
+
+    console.log("Event Place ve link:", place, e_link);
 
     badges.push(`[${place}](${baseURL}${e_link})`);
 
@@ -177,24 +212,33 @@ module.exports = {
     }
 
 
-    if(isApril) rank = "**Banned**"; 
-
     const embed = new MessageEmbed()
     .setColor(color)
-    .setDescription(`>>> User Stats\nRank: ${rank}\nXP: ${xp}\nKills: ${kills}\nDeaths: ${deaths}\nKDR: ${kdr}\nHighest Killstreak: ${hk}\nFavourite Weapon: ${weaponStats.mostKills}`)
+    .setDescription(`>>> ${e_title}\nRank: ${rank}\nXP: ${xp}\nKills: ${kills}\nDeaths: ${deaths}\nKDR: ${kdr}\nHighest Killstreak: ${hk}\nFavourite Weapon: ${weaponStats.mostKills}`)
     .setThumbnail(`${baseURL}/${image}`)
+   
     .setFooter({ text: footer });
 
+    console.log("CLANS:", clans);
     // clan section false değilse clanları göster
     if(clans[0] != false) embed.description += `\nClan(s): ${clans.join(' ')}`;
     // badgeler varsa badgeleri göster
     if(badges.length) embed.description += `\nAchievements/Victories:\n${badges.join('\n')}`;
+    // previousNames
+    console.log("veri:", userNames)
+
+    if(userNames.data && userNames.status == true && userNames.data.previousNames.length) { 
+    
+      embed.description += `\nPrevious Names:\n${userNames.data.previousNames.join(', ')}`
+
+    }
 
     let displayMsg;
 
     Number(userName) ? displayMsg = `${url} (${profileName})` : displayMsg = url;
 
     constants.interaction.editReply({ content: displayMsg, embeds: [embed] });
+  
 
     }); // listeItems biter
 
@@ -228,6 +272,7 @@ module.exports = {
         const u_name = $(element).find('a').text().trim();
         const u_link = $(element).find('a').attr('href');
 
+        console.log("link:", u_link)
 
         foundUsers.push(`• [${u_name}](${baseURL}${u_link})`);
 
@@ -243,6 +288,7 @@ module.exports = {
         .setColor('GREEN')
         .addField("Search Results", foundUsers.join('\n').toString())
         .setTimestamp();
+
 
         if(Number(n) > 10) { embed.setTitle(`${n} match`); embed.setURL(path) }
 
